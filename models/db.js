387afa14d761990f04c0786157697833d4d7533a
hanbin8269 <hanbin8269@gmail.com>
2020-11-28 19:20:30 +0900

    Add environment config option

--- models/db.js
+++ models/db.js
@@ -1,7 +1,11 @@
 var mysql = require('mysql');
 
+require('dotenv').config();
+
+
 var connection = mysql.createConnection({
     host     : process.env.DB_HOST,
+    port     : 3306,
     user     : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DATABASE

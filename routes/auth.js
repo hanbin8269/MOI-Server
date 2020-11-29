b8041d8670a197b82d53a5968af24856fe25682e
hanbin8269 <hanbin8269@gmail.com>
2020-11-29 16:46:47 +0900

    Feature Login errorhandling, cookie setting

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,48 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/userDAO');
 var crypto = require('crypto');
+const { PrismaClient } = require("@prisma/client");
 
+const prisma = new PrismaClient()
+
+router.post('/login', async (req, res) => {
+
+    const resultUser = await prisma.user.findUnique({
+        where:{
+            email : req.body.email
+        }
+    })
+
+    if (resultUser) {
+        res.status(400).send({'error' : "doesn' exist user"});
+        res.end();
+    }
+
+    // password 해싱
+    const hashedPassword = await new Promise((resolve, reject) => {
+        crypto.pbkdf2(req.body.password, process.env.SECRET_KEY, 100000, 64, 'sha512', (err, key) => {
+            if (err){
+                reject(err);
+            }
+            resolve(key.toString('base64'));
+        })
+    });
+
+    // password 일치 확인
+    if (resultUser.password != hashedPassword){
+        res.status(400).json({'error':"incorrect password"});
+    }
+    
+    // cookie setting
+    res.cookie("user", req.body.email, {
+        expires : new Date(Date.now() + 900000),
+        httpOnly:true
+    })
+    .status(200)
+    .send({'message' : "login success"})
+    .end();
+
+})
 
 router.post('/sign-up', async (req, res) =>{
 
@@ -25,7 +66,6 @@ router.post('/sign-up', async (req, res) =>{
     })
     res.render('index', {
         title : 'hello',
-        name : hashedPassword,
         length: 1
     });
 })

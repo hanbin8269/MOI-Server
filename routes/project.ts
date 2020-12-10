42d001d1847c7477ee232e9fbfdc6fb146ac2f51
hanbin8269 <hanbin8269@gmail.com>
2020-12-10 10:03:28 +0900

    Change javascript to typescript

--- routes/project.ts
+++ routes/project.ts
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var crypto = require('crypto');
 const { PrismaClient } = require("@prisma/client");
 const { body, validationResult } = require('express-validator');
 
 const prisma = new PrismaClient()
 
-router.post('/project',async(req, res) =>{
+router.post('/', async(req, res) =>{
     // 인증된 사용자인지 검사
-
+    console.log(req.body.users)
+    const user_list = await req.body.users.map(user => prisma.User.findUnique({
+        where : {
+            email : user
+        }
+    }));
+    console.log(user_list);
     // users id 배열로 User 배열 생성
         // 존재하지 않는 User라면 `Does not exist User`
     // techs id 배열로 Tech 배열 생성
@@ -16,7 +21,7 @@ router.post('/project',async(req, res) =>{
 
     // 시간 되면 링크 유효성 검사
 
-    project_result = await prisma.Project.create({
+    const project_result = await prisma.Project.create({
         data : {
             title : req.body.title,
             content : req.body.content,
@@ -26,7 +31,13 @@ router.post('/project',async(req, res) =>{
         }
     })
 
-    res.send(result)
+    res.send(project_result)
     .status(201)
     .end();
-);
+})
+
+router.get('/project', async(req,res)=>{
+    
+})
+
+module.exports = router

7393c8150b805e170e00ee30d9790667c49fd23f
hanbin8269 <hanbin8269@gmail.com>
2020-12-12 02:17:07 +0900

    Create mysql connection code

---
+++ models/db.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DATABASE
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+
+module.exports = connection;

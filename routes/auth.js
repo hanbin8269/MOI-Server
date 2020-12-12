c3482e121e9d7e14cd58f455d97510b10e0229f9
hanbin8269 <hanbin8269@gmail.com>
2020-12-12 20:30:20 +0900

    Create login

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,14 @@ var model = require('../models/userDAO');
 var crypto = require('crypto');
 
 router.post('/login', async (req, res) => {
-
-    const resultUser = await model.findUserByEmail(req.body, (results)=>{
-        return results;
+    var resultUser = await new Promise((resolve, reject)=>{
+        model.findUserByEmail(req.body, (results)=>{
+            resolve(results);
+        })
     })
-    console.log(resultUser)
 
-    if (resultUser) {
-        res.status(400).send({'error' : "doesn' exist user"});
+    if (!resultUser) {
+        res.status(400).send({'error' : "doesn't exist user"});
         res.end();
     }
 
@@ -27,41 +27,13 @@ router.post('/login', async (req, res) => {
 
     // password 일치 확인
     if (resultUser.password != hashedPassword){
-        res.status(400).json({'error':"incorrect password"});
+        res.status(400).json({'error':"incorrect password"}).end();
     }
     
-    // cookie setting
-    res.cookie("user", req.body.email, {
-        expires : new Date(Date.now() + 900000),
-        httpOnly:true
-    })
-    .status(200)
+    res.status(200)
     .send({'message' : "login success"})
     .end();
 
-})
-
-router.post('/sign-up', async (req, res) =>{
-
-    const hashedPassword = await new Promise((resolve, reject) => {
-        crypto.pbkdf2(req.body.password, process.env.SECRET_KEY, 100000, 64, 'sha512', (err, key) => {
-            if (err){
-                reject(err);
-            }
-            resolve(key.toString('base64'));
-        })
-    });
-
-    await prisma.user.create({
-        data : {
-            email : req.body.email,
-            password : hashedPassword
-        },
-    });
-
-    res.status(201)
-    .send({"message" : "sign up success"})
-    .end();
 })
 
 
